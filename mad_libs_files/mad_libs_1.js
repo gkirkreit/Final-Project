fb6ee4d6f1a7c43bed6962e8d89d48a2c0adf629
kirkreit <gkirkreit001@gmail.com>
2016-12-09 18:09:32 -0500

    commit1

---
+++ mad_libs_files/mad_libs_1.js
@@ -0,0 +1,36 @@
+var adjective1 = "";
+var verbEndingInED = "";
+var nounPlural1 = "";
+var liquid = "";
+var nounPlural2 = "";
+var famousPerson = "";
+var place = "";
+var occupation = "";
+var noun1 = "";
+var nationality = "";
+var femaleCelebrity = "";
+var noun2 = "";
+var femaleFriend = "";
+var nounPlural3 = "";
+var number = "";
+var adjective2 = "";
+var inputArr = [adjective1, verbEndingInED, nounPlural1, liquid, nounPlural2, famousPerson, place, occupation, noun1, nationality, femaleCelebrity, noun2, femaleFriend, nounPlural3, number, adjective2];
+
+function getUserInput(){
+	adjective1 = document.getElementById("adjective1");
+	verbEndingInED = document.getElementById("verbEndingInED");
+	nounPlural1 = document.getElementById("nounPlural1");
+	liquid = document.getElementById("liquid");
+	nounPlural2 = document.getElementById("nounPlural2");
+	famousPerson = document.getElementById("famousPerson");
+	place = document.getElementById("place");
+	occupation = document.getElementById("occupation");
+	noun1 = document.getElementById("noun1");
+	nationality = document.getElementById("nationality");
+	femaleCelebrity = document.getElementById("femaleCelebrity");
+	noun2 = document.getElementById("noun2");
+	femaleFriend = document.getElementById("femaleFriend");
+	nounPlural3 = document.getElementById("nounPlural3");
+	number = document.getElementById("number");
+	adjective2 = document.getElementById("adjective2");
+}
